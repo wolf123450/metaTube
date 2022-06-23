328df92132f807ee2342c01d4f343bfb79e0d87c
wolf123450@gmail.com <wolf123450@gmail.com>
2022-06-23 20:19:43 +0000

    Moved into client subfolder

--- Client/src/index.js
+++ Client/src/index.js
@@ -12,6 +12,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+
+//TODO: test using express: https://stackoverflow.com/questions/48601008/beginner-node-react-mysql-querying-the-db-and-displaying-the-result
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
